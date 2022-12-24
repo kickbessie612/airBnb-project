d110fe1e0b6540226f9a122c1b95f5574b8974c0
Yuan Wang <yuanwang612@gmail.com>
2022-12-23 20:34:25 -0800

    login user complete

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,16 @@
+// frontend/src/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
+
 import './index.css';
+
 import App from './App';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -14,6 +19,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
---
+++ frontend/src/store/session.js
@@ -0,0 +1,48 @@
+import { csrfFetch } from './csrf';
+
+const SET_USER = 'user/setUser';
+const REMOVE_USER = 'user/removeUser';
+
+export const setUser = user => {
+  return {
+    type: SET_USER,
+    user
+  };
+};
+
+export const removeUser = () => {
+  return {
+    type: REMOVE_USER
+  };
+};
+
+export const loginUser = (credential, password) => async dispatch => {
+  const res = await csrfFetch('/api/session', {
+    method: 'POST',
+    body: JSON.stringify({
+      credential,
+      password
+    })
+  });
+
+  if (res.ok) {
+    const data = await res.json();
+    dispatch(setUser(data.user));
+  }
+};
+
+const sessionReducer = (state = { user: null }, action) => {
+  const newState = { ...state };
+  switch (action.type) {
+    case SET_USER:
+      newState.user = action.user;
+      return newState;
+    case REMOVE_USER:
+      newState.user = null;
+      return newState;
+    default:
+      return newState;
+  }
+};
+
+export default sessionReducer;
