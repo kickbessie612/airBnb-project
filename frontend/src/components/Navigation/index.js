c0ad55e43873a4e8888d9a60c3e7bb9f13337f7d
Yuan Wang <yuanwang612@gmail.com>
2023-01-09 23:17:28 -0800

    add demo user button

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,13 +1,21 @@
 // frontend/src/components/Navigation/index.js
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginForm from '../LoginForm';
+import * as sessionActions from '../../store/session';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
+  const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
+
+  const loginDemo = () => {
+    dispatch(
+      sessionActions.loginUser({ credential: 'TenRings', password: 'password' })
+    );
+  };
 
   let sessionLinks;
   if (sessionUser) {
@@ -21,6 +29,7 @@ function Navigation({ isLoaded }) {
       <>
         <LoginForm />
         <NavLink to='/signup'>Sign Up</NavLink>
+        <button onClick={loginDemo}>Demo User</button>
       </>
     );
   }
