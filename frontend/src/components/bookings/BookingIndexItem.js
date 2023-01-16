9dc2512670284027fa2bb6367fa6de607c6473e0
Yuan Wang <yuanwang612@gmail.com>
2023-01-15 20:14:03 -0800

    add error handling when spot owner delete a booking

--- frontend/src/components/bookings/BookingIndexItem.js
+++ frontend/src/components/bookings/BookingIndexItem.js
@@ -1,18 +1,29 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteBooking } from '../../store/bookings';
+import { useState } from 'react';
 
 const BookingIndexItem = ({ booking, spot }) => {
   const sessionUser = useSelector(state => state.session.user);
   const dispatch = useDispatch();
+  const [errors, setErrors] = useState([]);
 
   const handleDelete = async e => {
     e.preventDefault();
+    setErrors([]);
     if (!window.confirm('Do you want to delete this booking?')) return;
-    await dispatch(deleteBooking(booking.id));
+    dispatch(deleteBooking(booking.id)).catch(async res => {
+      const data = await res.json();
+      if (data && data.errors) setErrors(data.errors);
+    });
   };
 
   return (
     <>
+      <ul>
+        {errors.map((error, idx) => (
+          <li key={idx}>{error}</li>
+        ))}
+      </ul>
       <div>
         {booking.startDate} to {booking.endDate}
       </div>
